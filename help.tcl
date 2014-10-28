d0ecf201aad0660150c493b8ead29417806fa7e1
cr0n <cr0n@cypherpunks.cc>
2014-10-28 16:40:21 +0100

    more fancy borders for help screen

--- help.tcl
+++ help.tcl
@@ -14,51 +14,51 @@ bind msg - !chelp msg:calhelp
 proc int:print_header {n} {
     global botvers
     puthelp "PRIVMSG $n :factbot v$botvers"
-    puthelp "PRIVMSG $n :==============================================================================================="
-    puthelp "PRIVMSG $n :channel cmd      | desc"
-    puthelp "PRIVMSG $n :-----------------+-----------------------------------------------------------------------------"
+    puthelp "PRIVMSG $n :━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    puthelp "PRIVMSG $n :channel cmd                        │ desc"
+    puthelp "PRIVMSG $n :───────────────────────────────────┼─────────────────────────────────────────────"
 }
 
 proc int:print_footer {n} {
     global cmdurl
-    puthelp "PRIVMSG $n :==============================================================================================="
-    puthelp "PRIVMSG $n :for a complete list of commands visit: $cmdurl"
+    puthelp "PRIVMSG $n :━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    puthelp "PRIVMSG $n :complete list of commands: $cmdurl"
 }
 
 proc msg:hhelp {n u h a} {
     int:print_header $n
-    puthelp "PRIVMSG $n :!hup             | sets status to 'up' (disables auto topic)"
-    puthelp "PRIVMSG $n :!hdown           | sets status to 'down' (disables auto topic)"
-    puthelp "PRIVMSG $n :!htoggle         | toggles status (disables auto topic)"
-    puthelp "PRIVMSG $n :!hinit           | (re)enables auto topic"
-    puthelp "PRIVMSG $n :!hauto           | idem"
-    puthelp "PRIVMSG $n :!hstop           | disables auto topic"
-    puthelp "PRIVMSG $n :!hhelp           | guess what?"
+    puthelp "PRIVMSG $n :!hup                               │ sets status to 'up' (disables auto topic)"
+    puthelp "PRIVMSG $n :!hdown                             │ sets status to 'down' (disables auto topic)"
+    puthelp "PRIVMSG $n :!htoggle                           │ toggles status (disables auto topic)"
+    puthelp "PRIVMSG $n :!hinit                             │ (re)enables auto topic"
+    puthelp "PRIVMSG $n :!hauto                             │ idem"
+    puthelp "PRIVMSG $n :!hstop                             │ disables auto topic"
+    puthelp "PRIVMSG $n :!hhelp                             │ guess what?"
     int:print_footer $n
 }
 
 proc msg:cdbhelp {n u h a} {
     int:print_header $n
-    puthelp "PRIVMSG $n :!fact <#>        | get fact num <#>"
-    puthelp "PRIVMSG $n :!add <fact>      | add <fact>"
-    puthelp "PRIVMSG $n :factbot: <fact>  | idem"
-    puthelp "PRIVMSG $n :!del <#>         | delete fact num <#>"
-    puthelp "PRIVMSG $n :!grep <regex>    | search facts for <regex>"
-    puthelp "PRIVMSG $n :!help            | guess what?"
-    puthelp "PRIVMSG $n :!randfact        | get a random fact"
+    puthelp "PRIVMSG $n :!fact <#>                          │ get fact num <#>"
+    puthelp "PRIVMSG $n :!add <fact>                        │ add <fact>"
+    puthelp "PRIVMSG $n :factbot: <fact>                    │ idem"
+    puthelp "PRIVMSG $n :!del <#>                           │ delete fact num <#>"
+    puthelp "PRIVMSG $n :!grep <regex>                      │ search facts for <regex>"
+    puthelp "PRIVMSG $n :!help                              │ guess what?"
+    puthelp "PRIVMSG $n :!randfact                          │ get a random fact"
     int:print_footer $n
 }
 
 proc msg:calhelp {n u h a} {
     int:print_header $n
-    puthelp "PRIVMSG $n :!addevent <date> HH?:MM foo bar ...   | add event 'foo bar ...' at <date> HH?:MM"
-    puthelp "PRIVMSG $n :!delevent <date> HH?:MM foo bar       | delete event 'foo bar' (must be exact) at <date> HH?:MM"
-    puthelp "PRIVMSG $n :!what <date>                          | get all events at <date>"
-    puthelp "PRIVMSG $n :!when foo ...                         | get the scheduled date for 'foo ...'"
-    puthelp "PRIVMSG $n :!whatnext                             | get next event"
-    puthelp "PRIVMSG $n :!whatweek                             | get all events in the current week"
-    puthelp "PRIVMSG $n :!calurl                               | get the url for the calendar (.ics format)"
-    puthelp "PRIVMSG $n :!chelp                                | guess what?"
+    puthelp "PRIVMSG $n :!addevent <date> HH?:MM foo bar    │ add event 'foo bar' at <date> HH?:MM"
+    puthelp "PRIVMSG $n :!delevent <date> HH?:MM foo bar    │ delete event 'foo bar' at <date> HH?:MM"
+    puthelp "PRIVMSG $n :!what <date>                       │ get all events at <date>"
+    puthelp "PRIVMSG $n :!when foo bar                      │ get the scheduled date for 'foo bar'"
+    puthelp "PRIVMSG $n :!whatnext                          │ get next event"
+    puthelp "PRIVMSG $n :!whatweek                          │ get all events in the current week"
+    puthelp "PRIVMSG $n :!calurl                            │ get the url for the calendar (.ics format)"
+    puthelp "PRIVMSG $n :!chelp                             │ guess what?"
     int:print_footer $n
 }
 
