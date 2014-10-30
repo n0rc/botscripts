b5089fb7c66c9ecb3529514e82d8c09f41a06924
cr0n <cr0n@cypherpunks.cc>
2014-10-30 10:27:10 +0100

    refactoring of help functions

--- help.tcl
+++ help.tcl
@@ -1,56 +1,54 @@
 # eggdrop help functions
 # (c)2014 cr0n
 
+set vers "0.1"
 set botvers "0.4"
 set cmdurl "http://url.with.further.info"
 
-bind pub - !help pub:cdbhelp
-bind msg - !help msg:cdbhelp
-bind pub - !hhelp pub:hhelp
-bind msg - !hhelp msg:hhelp
-bind pub - !chelp pub:calhelp
-bind msg - !chelp msg:calhelp
+bind pub - !help pub:help
+bind msg - !help msg:help
 
-proc int:print_header {n} {
+proc int:print:header {n} {
     global botvers
     puthelp "PRIVMSG $n :factbot v$botvers"
     puthelp "PRIVMSG $n :━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-    puthelp "PRIVMSG $n :channel cmd                        │ desc"
+    puthelp "PRIVMSG $n :channel command                    │ description"
     puthelp "PRIVMSG $n :───────────────────────────────────┼─────────────────────────────────────────────"
 }
 
-proc int:print_footer {n} {
+proc int:print:footer {n} {
     global cmdurl
     puthelp "PRIVMSG $n :━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
     puthelp "PRIVMSG $n :complete list of commands: $cmdurl"
 }
 
-proc msg:hhelp {n u h a} {
-    int:print_header $n
+
+proc int:print:help:general {n} {
+    puthelp "PRIVMSG $n :!help calendar                     │ get help for calendar functions"
+    puthelp "PRIVMSG $n :!help facts                        │ get help for fact functions"
+    puthelp "PRIVMSG $n :!help status                       │ get help for status functions"
+}
+
+proc int:print:help:status {n} {
     puthelp "PRIVMSG $n :!hup                               │ sets status to 'up' (disables auto topic)"
     puthelp "PRIVMSG $n :!hdown                             │ sets status to 'down' (disables auto topic)"
     puthelp "PRIVMSG $n :!htoggle                           │ toggles status (disables auto topic)"
     puthelp "PRIVMSG $n :!hinit                             │ (re)enables auto topic"
     puthelp "PRIVMSG $n :!hauto                             │ idem"
     puthelp "PRIVMSG $n :!hstop                             │ disables auto topic"
-    puthelp "PRIVMSG $n :!hhelp                             │ guess what?"
-    int:print_footer $n
+    puthelp "PRIVMSG $n :!trollenv                          │ get temperature and radioactivity in hoehle"
 }
 
-proc msg:cdbhelp {n u h a} {
-    int:print_header $n
+proc int:print:help:facts {n} {
     puthelp "PRIVMSG $n :!fact <#>                          │ get fact num <#>"
     puthelp "PRIVMSG $n :!add <fact>                        │ add <fact>"
     puthelp "PRIVMSG $n :factbot: <fact>                    │ idem"
     puthelp "PRIVMSG $n :!del <#>                           │ delete fact num <#>"
     puthelp "PRIVMSG $n :!grep <regex>                      │ search facts for <regex>"
-    puthelp "PRIVMSG $n :!help                              │ guess what?"
     puthelp "PRIVMSG $n :!randfact                          │ get a random fact"
-    int:print_footer $n
 }
 
-proc msg:calhelp {n u h a} {
-    int:print_header $n
+proc int:print:help:calendar {n} {
     puthelp "PRIVMSG $n :!addevent <date> HH?:MM foo bar    │ add event 'foo bar' at <date> HH?:MM"
     puthelp "PRIVMSG $n :!delevent <date> HH?:MM foo bar    │ delete event 'foo bar' at <date> HH?:MM"
     puthelp "PRIVMSG $n :!what <date>                       │ get all events at <date>"
@@ -58,20 +56,29 @@ proc msg:calhelp {n u h a} {
     puthelp "PRIVMSG $n :!whatnext                          │ get next event"
     puthelp "PRIVMSG $n :!whatweek                          │ get all events in the current week"
     puthelp "PRIVMSG $n :!calurl                            │ get the url for the calendar (.ics format)"
-    puthelp "PRIVMSG $n :!chelp                             │ guess what?"
-    int:print_footer $n
 }
 
-proc pub:calhelp {n u h c a} {
-    msg:calhelp $n $u $h $a
+proc msg:help {n u h a} {
+    int:print:header $n
+    if {[llength $a] == 1} {
+        set what [lindex $a 0]
+        if {$what == "calendar"} {
+            int:print:help:calendar $n
+        } elseif {$what == "facts"} {
+            int:print:help:facts $n
+        } elseif {$what == "status"} {
+            int:print:help:status $n
+        } else {
+            int:print:help:general $n
+        }
+    } else {
+        int:print:help:general $n
+    }
+    int:print:footer $n
 }
 
-proc pub:cdbhelp {n u h c a} {
-    msg:cdbhelp $n $u $h $a
+proc pub:help {n u h c a} {
+    msg:help $n $u $h $a
 }
 
-proc pub:hhelp {n u h c a} {
-    msg:hhelp $n $u $h $a
-}
-
-putlog "Help functions successfully loaded."
+putlog "Help functions v$vers successfully loaded."
